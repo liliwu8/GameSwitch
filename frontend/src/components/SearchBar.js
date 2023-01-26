5f8d88c279cebd531ca5534d6b877c7cbe5281a9
lily08 <liliangelxd@gmail.com>
2023-01-26 13:05:13 -0500

    fix no result on search bar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,35 +1,53 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import './SearchBar.css'
-const API = process.env.REACT_APP_API_URL
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './SearchBar.css';
+const API = process.env.REACT_APP_API_URL;
 
 function SearchBar() {
-  const [games, setGames] = useState([])
-  const [userInput, setUserInput] = useState('')
+  const [games, setGames] = useState([]);
+  const [userInput, setUserInput] = useState('');
 
   useEffect(() => {
     axios
       .get(`${API}/games`)
       .then((res) => {
-        setGames(res.data.payload)
+        setGames(res.data.payload);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+        console.log(err);
+      });
+  }, []);
 
   const inputHandler = (event) => {
-    let textLowerCase = event.target.value.toLowerCase()
-    setUserInput(textLowerCase)
-  }
+    let textLowerCase = event.target.value.toLowerCase();
+    setUserInput(textLowerCase);
+  };
 
   const SearchHandleClick = (input) => {
-    setUserInput(input)
+    setUserInput(input);
     // console.log('input is', input)
-    setUserInput('')
-  }
+    setUserInput('');
+  };
 
+  const searchResult = (userInput) => {
+    let filterGame = games.filter((game) =>
+      game.game_name.toLowerCase().includes(userInput)
+    );
+    return filterGame.length
+      ? filterGame.map((game, index) => (
+          <ul
+            className='dropdown-row'
+            key={index}
+            onClick={() => SearchHandleClick(game.game_name)}
+          >
+            <li>
+              <Link to={`/games/${game.game_id}`}>{game.game_name}</Link>
+            </li>
+          </ul>
+        ))
+      : <ul><li>No Result</li></ul>
+  };
   return (
     <div className='search-bar'>
       <input
@@ -40,27 +58,10 @@ function SearchBar() {
         className='form-input'
       />
       <div className='dropdown'>
-        {userInput.length > 0
-          ? games
-              .filter((game) =>
-                game.game_name.toLowerCase().includes(userInput)
-              )
-              .map((game, index) => (
-                <ul
-                  className='dropdown-row'
-                  key={index}
-                  onClick={() => SearchHandleClick(game.game_name)}
-                >
-                  <li>
-                    <Link to={`/games/${game.game_id}`}>{game.game_name}</Link>
-                  </li>
-                </ul>
-              ))
-          : null}{
-          }
+        {userInput.length > 0 ? searchResult(userInput) : null}
       </div>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
