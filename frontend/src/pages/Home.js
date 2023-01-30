76b3a0759ac2c464a84005d9b35bd7d03fa905c1
lily08 <liliangelxd@gmail.com>
2023-01-30 15:43:30 -0500

    fix spelling on home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,32 +8,34 @@ function Home() {
       <div className='banner-section'>
         <img
           src='https://static.standard.co.uk/2020/12/07/11/topgames2020compositejpg?width=1024&auto=webp&quality=50&crop=968%3A645%2Csmart'
-          className='home-image' alt='pic'
+          className='home-image'
+          alt='pic'
         />
         <section class='hero-header-text'>
           <h1 className='home-title'>Welcome To Game Switch NYC!</h1>
-          <h2 className='slogan'>Game Connect Repeat</h2>
         </section>
       </div>
       <div className='features-container'>
         <div className='feature'>
           <h2 className='feature-title'>Game</h2>
           <p className='feature-description'>
-            Create your own gaming library to trade with local users. View and manage your library.
+            Create your own gaming library to trade with local users. View and
+            manage your library.
           </p>
         </div>
         <div className='feature'>
           <h2 className='feature-title'>Connect</h2>
           <p className='feature-description'>
-            Connect with local peers and trading video games or
-            meet up in person. Connect with like-minded users.
+            Connect with local peers and trading video games or meet up in
+            person. Connect with like-minded users.
           </p>
         </div>
         <div className='feature'>
           <h2 className='feature-title'>Trade</h2>
           <p className='feature-description'>
             Trading games with other users can be a great way to get rid of
-            games you no longer want, and expand your library while saving money.
+            games you no longer want, and expand your library while saving
+            money.
           </p>
         </div>
       </div>
